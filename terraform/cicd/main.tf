8ac14feb20e08030f8fc306b2efc3a8464985c25
Rafał Gołąb <rafal.golab@gmail.com>
2021-08-08 07:10:30 +0200

    Extend role for SA (add instanceAdmin)

--- terraform/cicd/main.tf
+++ terraform/cicd/main.tf
@@ -66,7 +66,8 @@ locals {
     "roles/logging.configWriter",
     "roles/resourcemanager.projectCreator",
     "roles/resourcemanager.folderAdmin",
-    "roles/serviceusage.serviceUsageAdmin"
+    "roles/serviceusage.serviceUsageAdmin",
+    "roles/compute.instanceAdmin",
   ]
   cloudbuild_devops_roles = [
     # Allow CICD to view all resources within the devops project so it can run terraform plans against them.
